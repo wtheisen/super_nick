0911f0665ad3839b4abae47753a530ee3af77329
wtheisen <wtheisen@nd.edu>
2019-08-22 13:34:13 -0400

    Actual code file

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # super_nick
-A weechat plugin to have a root nicklist
+A weechat plugin to have a root nicklist.
+One of the most frustrating things about weechat is that each window has its
+own nicklist. I couldn't figure out how to do a single nicklist that has the
+nicks for the window in focus and so I wrote this plugin to do so. It still
+needs some polish but the basic functionality is there.
---
+++ super_nick.py
@@ -0,0 +1,40 @@
+import weechat as w
+
+w.register("super_nick", "FlashCode", "1.0", "GPL3", "Test script", "", "")
+
+def build_list(data, item, window):
+    currBuffer = w.current_buffer()
+
+    nicklist = {}
+    infolist_nicklist = w.infolist_get('nicklist', currBuffer, '')
+    while w.infolist_next(infolist_nicklist):
+        nick = w.infolist_string(infolist_nicklist, 'name')
+        prefix = w.infolist_string(infolist_nicklist, 'prefix')
+        nick_type = w.infolist_string(infolist_nicklist, 'type')
+        if nick_type != 'nick':
+            pass
+        else:
+            if not nicklist.has_key(prefix):
+                nicklist[prefix]=[]
+            nicklist[prefix].append(nick)
+
+    nickListString = ''
+    for p,l in nicklist.items():
+        nickListString += ''.join(['{}{}\n'.format(p, n) for n in l])
+    print(nickListString)
+    return nickListString
+
+def update_super_nick(data, signal, signal_data):
+    # print('buffer switched')
+    w.bar_item_update('super_nick_list')
+    return w.WEECHAT_RC_OK
+
+super_nick_list = w.bar_item_new('super_nick_list', 'build_list', '')
+super_nick_bar = w.bar_new("super_nick", "off", "100", "root", "", "right", "vertical", "vertical",
+    "15", "15", "default", "cyan", "black", "on", 'super_nick_list')
+
+h = w.hook_signal('buffer_switch', 'update_super_nick', '')
+h = w.hook_signal('window_switch', 'update_super_nick', '')
+h = w.hook_signal('irc_channel_opened', 'update_super_nick', '')
+
+print('super_nick loaded...')
